9121fff2d503ea6497f0f7fc3031e8ae831eb0c0
Andreas Retzius <andreasretzius@gmail.com>
2023-11-29 11:25:40 +0100

    .env api key

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //only worked with vite..
-axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=6424148a&s=matrix")
+axios.get("http://www.omdbapi.com/?i=tt3896198&apikey="+import.meta.env.VITE_MY_API_KEY+"&s=matrix")
 .then((response)=>{
 response.data;
 console.log(response);
@@ -9,7 +9,7 @@ console.log(response);
 //axios async await;
 
 window.addEventListener("load", async () =>{
-    const response2 = await axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=6424148a&s=matrix");
+    const response2 = await axios.get("http://www.omdbapi.com/?i=tt3896198&apikey="+import.meta.env.VITE_MY_API_KEY+"&s=matrix");
 response2.data;
 
 console.log(response2);
